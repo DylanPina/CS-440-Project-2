b6584a09bf4fda695b84bf2226cc8561beb02560
Dylan Pina <dillon_idgafos@yahoo.com>
2023-11-08 12:50:36 -0500

    Added missing log statements to bot 3 in action

--- code/bots/probablistic_bots/bot_three.py
+++ code/bots/probablistic_bots/bot_three.py
@@ -6,7 +6,7 @@ from config import Bots
 class BotThree(ProbabilisticBot):
     """
     All cells (other than the bot's initial cell) start with equal probability of containing the leak. The nearer the bot is to the leak, the more likely it is to receive a beep. Bot 3 proceeds in the following way:
-    
+
         - At any time, the bot is going to move to the cell that has the highest probability of containing the leak (breaking ties first by distance from the bot, then at random).
         - After entering any cell, if the cell does not contain a leak, the bot will take the sense action. Based on the results, it updates the probability of containing the leak for each cell.
         - After the beliefs are updated, this repeats, the bot proceeding to the cell that as the highest probability of containing the leak (breaking ties first by distance from the bot, then at random).
@@ -23,5 +23,7 @@ class BotThree(ProbabilisticBot):
         logging.debug(f"Timestep: {timestep}")
         if timestep % 2:
             self.move()
+            self.moves += 1
         else:
             self.sense()
+            self.senses += 1
